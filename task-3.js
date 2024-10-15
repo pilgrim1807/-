873c5afb920897e9c389c55269269b3f405047ea
pilgrim1807 <51112693+pilgrim1807@users.noreply.github.com>
2024-10-15 17:27:42 +0300

    Ответ на задание #3

---
+++ task-3.js
@@ -0,0 +1,8 @@
+ console.log(String(console.log()));
+console.log(Number({ name: 'Maxim' }));
+console.log(Boolean(Symbol('key')));
+console.log(String(Number));
+console.log(Number(''));
+console.log(String(0));
+console.log(Boolean(-10));
+console.log(Number('-105'));
