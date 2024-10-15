993bfd6bad78aa8217a71081e7e245d0024f4f84
pilgrim1807 <51112693+pilgrim1807@users.noreply.github.com>
2024-10-15 17:22:56 +0300

    Ответ на задание #1

---
+++ index.js
@@ -0,0 +1,28 @@
+let row = 'ряд';
+console.log(Boolean(row), String(row), Number(row),);
+
+let degree = 36;
+console.log(Number(degree), Boolean(degree),String(degree));
+
+let right = true; 
+console.log(Number(right), String(right), Boolean(right));
+
+let highIndicator = 997000254873542167999999;
+console.log(Number(highIndicator), String(highIndicator), Boolean(highIndicator));
+
+let index = null; 
+console.log(Number(index), Boolean(index), String(index));
+
+let obscurity;
+console.log(Boolean(obscurity), String(index), Number(obscurity));
+
+let sphere = {
+    shape: "round",
+    volume: 28,
+    weight: 54,
+    аccelerationOfGravity: 9.80665
+}
+console.log(Boolean(sphere), Number(sphere), String(sphere));
+
+let sign = specific("sign");
+console.log(Boolean(sign), String(sign));
